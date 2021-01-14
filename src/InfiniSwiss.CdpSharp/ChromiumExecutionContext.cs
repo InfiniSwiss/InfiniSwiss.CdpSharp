77658c07720591f794c6b07606fddd5b42788db8
Sandro Hanea <sandro@shane.ro>
2021-01-14 10:50:31 +0200

    Fixed an issue which caused the app to crash if the chromium process exited before disposing.

--- src/InfiniSwiss.CdpSharp/ChromiumExecutionContext.cs
+++ src/InfiniSwiss.CdpSharp/ChromiumExecutionContext.cs
@@ -33,7 +33,10 @@ namespace InfiniSwiss.CdpSharp
                 this.cdpCommunicator.Dispose();
             }
 
-            this.chromiumProcess?.Kill();
+            if (!chromiumProcess.HasExited)
+            {
+                this.chromiumProcess?.Kill();
+            }
 
             disposed = true;
         }
